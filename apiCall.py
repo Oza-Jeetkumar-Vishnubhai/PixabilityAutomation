5fe4d0a38686692278817ec69ad9daf4cc3c15eb
Jeet Oza <onlyjeet31@gmail.com>
2024-04-24 13:25:19 +0530

    url change

--- apiCall.py
+++ apiCall.py
@@ -3,10 +3,10 @@ from prepareDeck import prepareDeck
 app = Flask(__name__)
 
 # Define a route for the GET request
-@app.route('/abc', methods=['GET'])
+@app.route('/', methods=['GET'])
 def hello():
     prepareDeck()
     return jsonify({'message': 'Hello, World!'})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False)
